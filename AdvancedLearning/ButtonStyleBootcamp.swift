634cdc5c04a3c46601bd648ab72d6526bc60dacb
Aeshma <madhushanka2727@gmail.com>
2023-12-08 20:55:00 +0530

    use previous text style for the button label

--- AdvancedLearning/ButtonStyleBootcamp.swift
+++ AdvancedLearning/ButtonStyleBootcamp.swift
@@ -36,12 +36,7 @@ struct ButtonStyleBootcamp: View {
         }, label: {
             Text("click me")
                 .font(.headline)
-                .foregroundStyle(.white)
-                .frame(height: 55)
-                .frame(maxWidth: .infinity)
-                .background(Color.blue)
-                .cornerRadius(10)
-                .shadow(color: .blue.opacity(0.3), radius: 10, x: 0.0, y: 10)
+                .withDefaultButtonFormatting()
         })
 //        .buttonStyle(PressableButtonStyle())
         .withPressableStyle()
