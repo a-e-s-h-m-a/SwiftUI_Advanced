f0ff02443c5789cf8bb2c17b9ea2141a2e1c5ca3
Aeshma <madhushanka2727@gmail.com>
2023-12-09 23:06:00 +0530

    add the basic setup

---
+++ AdvancedLearning/AnyTransitionBootcamp.swift
@@ -0,0 +1,39 @@
+//
+//  AnyTransitionBootcamp.swift
+//  AdvancedLearning
+//
+//  Created by Malith Madhushanka on 2023-12-09.
+//
+
+import SwiftUI
+
+struct AnyTransitionBootcamp: View {
+    
+    @State private var showRectangle: Bool = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            if showRectangle {
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(width: 250, height: 350)
+            }
+            
+            Spacer()
+            
+            Text("Click me!")
+                .withDefaultButtonFormatting()
+                .padding(.horizontal, 40)
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        showRectangle.toggle()
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    AnyTransitionBootcamp()
+}
