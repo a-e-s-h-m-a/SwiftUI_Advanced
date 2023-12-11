4bf800939b0ad2f51f892fde45b0fd7201de2e26
Aeshma <madhushanka2727@gmail.com>
2023-12-11 20:38:42 +0530

    animate single corner - wont work

--- AdvancedLearning/AnimatableDataBootcamp.swift
+++ AdvancedLearning/AnimatableDataBootcamp.swift
@@ -13,7 +13,8 @@ struct AnimatableDataBootcamp: View {
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: animate ? 60 : 0)
+//            RoundedRectangle(cornerRadius: animate ? 60 : 0)
+            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 60 : 0)
                 .frame(width: 250, height: 250)
         }
         .onAppear {
@@ -26,4 +27,28 @@ struct AnimatableDataBootcamp: View {
 
 #Preview {
     AnimatableDataBootcamp()
+}
+
+struct RectangleWithSingleCornerAnimation: Shape {
+    
+    var cornerRadius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            
+            path.addArc(
+                center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                radius: cornerRadius,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 360),
+                clockwise: false
+            )
+            
+            path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
 }
