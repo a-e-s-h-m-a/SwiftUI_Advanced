71e9d22a9a9a704ac23eb0c742552143d42d6047
Aeshma <madhushanka2727@gmail.com>
2023-12-11 12:58:53 +0530

    custom shapes with lines

---
+++ AdvancedLearning/CustomShapesBootcamp.swift
@@ -0,0 +1,74 @@
+//
+//  CustomShapesBootcamp.swift
+//  AdvancedLearning
+//
+//  Created by Malith Madhushanka on 2023-12-11.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
+struct CustomShapesBootcamp: View {
+    var body: some View {
+        ZStack {
+//            Image("dog")
+//                .resizable()
+//                .scaledToFill()
+            
+            
+            Trapezoid()
+//            Diamond()
+//            Triangle()
+//                .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, dash: [10]))
+//                .foregroundColor(.blue)
+//                .trim(from: 0, to: 0.5)
+//                .fill(LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                .frame(width: 300, height: 100)
+//                .clipShape(Triangle().rotation(Angle(degrees: 180)))
+        }
+    }
+}
+
+#Preview {
+    CustomShapesBootcamp()
+}
