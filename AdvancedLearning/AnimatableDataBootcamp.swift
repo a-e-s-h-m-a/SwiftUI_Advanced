d1462909317716bdc60f32ee6f3b7373e517982f
Aeshma <madhushanka2727@gmail.com>
2023-12-11 20:53:13 +0530

    animate packman shape - animatable data

--- AdvancedLearning/AnimatableDataBootcamp.swift
+++ AdvancedLearning/AnimatableDataBootcamp.swift
@@ -14,11 +14,12 @@ struct AnimatableDataBootcamp: View {
     var body: some View {
         ZStack {
 //            RoundedRectangle(cornerRadius: animate ? 60 : 0)
-            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 60 : 0)
+//            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 60 : 0)
+            Pacman(offsetAmount: animate ? 20 : 0)
                 .frame(width: 250, height: 250)
         }
         .onAppear {
-            withAnimation(Animation.linear(duration: 2.0).repeatForever()) {
+            withAnimation(Animation.easeInOut.repeatForever()) {
                 animate.toggle()
             }
         }
@@ -54,6 +55,29 @@ struct RectangleWithSingleCornerAnimation: Shape {
             
             path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY))
             path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
+struct Pacman: Shape {
+    
+    var offsetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { offsetAmount }
+        set { offsetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.minY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: offsetAmount),
+                endAngle: Angle(degrees: -offsetAmount),
+                clockwise: false
+            )
         }
     }
 }
