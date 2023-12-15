c2346d76089a5e501b23bed08fcae8e70d93cef3
Aeshma <madhushanka2727@gmail.com>
2023-12-15 12:24:39 +0530

    custom tabview - add matched geometry sliding effect to the tab selection

--- AdvancedLearning/CustomTabBar/CustomTabBarView.swift
+++ AdvancedLearning/CustomTabBar/CustomTabBarView.swift
@@ -11,18 +11,10 @@ struct CustomTabBarView: View {
     
     let tabs: [TabBarItem]
     @Binding var selection: TabBarItem
+    @Namespace private var namespace
     
     var body: some View {
-        HStack {
-            ForEach(tabs, id: \.self) { tab in
-                tabview(tab: tab)
-                    .onTapGesture {
-                        switchToTab(tab: tab)
-                    }
-            }
-        }
-        .padding(6)
-        .background(.white)
+        tabBarV2
     }
 }
 
@@ -54,5 +46,57 @@ extension CustomTabBarView {
         withAnimation(.easeInOut) {
             selection = tab
         }
+    }
+    
+    private var tabBarV1: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabview(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(.white)
+    }
+}
+
+extension CustomTabBarView {
+    private func tabview2(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+                .font(.subheadline)
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .foregroundColor(selection == tab ? tab.color : .gray)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                if selection == tab {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(tab.color.opacity(0.2))
+                        .matchedGeometryEffect(id: "background_rectangle", in: namespace)
+                }
+            }
+        )
+    }
+    
+    private var tabBarV2: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabview2(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(.white)
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
+        .padding(.horizontal)
     }
 }
