6e75c594f9b158deb14f08fe17734caf4117def9
Aeshma <madhushanka2727@gmail.com>
2023-12-15 11:30:58 +0530

    custom tabview 4

--- AdvancedLearning/CustomTabBar/AppTabBarView.swift
+++ AdvancedLearning/CustomTabBar/AppTabBarView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 struct AppTabBarView: View {
     
     @State private var selection: String = "Home"
+    @State var tabSelection: TabBarItem = TabBarItem(iconName: "house", title: "Home", color: .red)
     
     var body: some View {
+        CustomTabBarContainerView(selection: $tabSelection) {
+            Color.blue
+        }
+//        defaultTabView
+    }
+}
+
+#Preview {
+    let tabs: [TabBarItem] = [
+        TabBarItem(iconName: "house", title: "Home", color: .red),
+        TabBarItem(iconName: "heart", title: "Favourites", color: .blue),
+        TabBarItem(iconName: "person", title: "Profile", color: .green)
+    ]
+    
+    return AppTabBarView()
+}
+
+extension AppTabBarView {
+    private var defaultTabView: some View {
         TabView(selection: $selection) {
             Color.red
                 .tabItem {
@@ -32,8 +52,4 @@ struct AppTabBarView: View {
                 }
         }
     }
-}
-
-#Preview {
-    AppTabBarView()
 }
