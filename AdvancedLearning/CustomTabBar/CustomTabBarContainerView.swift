4465e3d2e8c4accb74c8284a50b7eb80bca24f19
Aeshma <madhushanka2727@gmail.com>
2023-12-15 11:23:22 +0530

    custom tabview 3

---
+++ AdvancedLearning/CustomTabBar/CustomTabBarContainerView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomTabBarContainerView.swift
+//  AdvancedLearning
+//
+//  Created by Malith Madhushanka on 2023-12-15.
+//
+
+import SwiftUI
+
+struct CustomTabBarContainerView<Content: View>: View {
+    @Binding var selection: TabBarItem
+    let content: Content
+    @State private var tabs: [TabBarItem] = []
+    
+    init(selection: Binding<TabBarItem>, @ViewBuilder content: () -> Content) {
+        self._selection = selection
+        self.content = content()
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ZStack {
+                content
+            }
+            CustomTabBarView(tabs: tabs, selection: $selection)
+        }
+    }
+}
+
+#Preview {
+    let tabs: [TabBarItem] = [
+        TabBarItem(iconName: "house", title: "Home", color: .red),
+        TabBarItem(iconName: "heart", title: "Favourites", color: .blue),
+        TabBarItem(iconName: "person", title: "Profile", color: .green)
+    ]
+    
+    return CustomTabBarContainerView(selection: .constant(tabs.first!)) {
+        Color.red
+    }
+}
