2493b37de390c6bdbc29f84243e4bf6a1f2783c6
Aeshma <madhushanka2727@gmail.com>
2023-12-16 11:45:42 +0530

    add uitextfield

--- AdvancedLearning/UIViewRepresentableBootcamp.swift
+++ AdvancedLearning/UIViewRepresentableBootcamp.swift
@@ -9,16 +9,49 @@ import SwiftUI
 
 //Converts UIView from UIKit to SwiftUI
 struct UIViewRepresentableBootcamp: View {
+    
+    @State private var text: String = ""
+    
     var body: some View {
         VStack {
-            Text("hello world")
-            BacisUIViewRepresentable()
+            Text(text)
+            TextField("Type here...", text: $text)
+                .frame(height: 55)
+                .background(.gray)
+            
+            UITextFieldViewRepresentable()
+                .frame(height: 55)
+                .background(.gray)
         }
     }
 }
 
 #Preview {
     UIViewRepresentableBootcamp()
+}
+
+struct UITextFieldViewRepresentable: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> some UIView {
+        return getTextField()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    private func getTextField() -> UITextField {
+        let placeHolder = NSAttributedString(
+            string: "Type here...",
+            attributes: [
+                .foregroundColor: UIColor.red
+            ]
+        )
+        
+        let textField = UITextField(frame: .zero)
+        textField.attributedPlaceholder = placeHolder
+        return textField
+    }
 }
 
 struct BacisUIViewRepresentable: UIViewRepresentable {
