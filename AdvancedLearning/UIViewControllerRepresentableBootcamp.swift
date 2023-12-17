0b68d848bb4cd1c06a44b8c749be8e336de70a02
Aeshma <madhushanka2727@gmail.com>
2023-12-17 10:21:11 +0530

    bind data image picker vc

--- AdvancedLearning/UIViewControllerRepresentableBootcamp.swift
+++ AdvancedLearning/UIViewControllerRepresentableBootcamp.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct UIViewControllerRepresentableBootcamp: View {
     
     @State private var showScreen: Bool = false
+    @State private var image: UIImage? = nil
     
     var body: some View {
         VStack {
             Text("Hi")
+            
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            
             Button(action: {
                 showScreen.toggle()
             }, label: {
                 Text("Click here")
             })
             .sheet(isPresented: $showScreen, content: {
-                UIImagePickerViewControllerRepresentable()
+                UIImagePickerViewControllerRepresentable(image: $image)
 //                BasicUIViewControllerRepresentable(labelString: "new text")
             })
         }
@@ -32,14 +41,38 @@ struct UIViewControllerRepresentableBootcamp: View {
 }
 
 struct UIImagePickerViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let vc = UIImagePickerController()
         vc.allowsEditing = false
+        vc.delegate = context.coordinator
         return vc
     }
     
+    // from SwiftUI to UIkit
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         
+    }
+    
+    // from UIKit to SwiftUI
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(image: $image)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        @Binding var image: UIImage?
+        
+        init(image: Binding<UIImage?>) {
+            self._image = image
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let newImage = info[.originalImage] as? UIImage else { return }
+            image = newImage
+        }
     }
 }
 
@@ -54,7 +87,6 @@ struct BasicUIViewControllerRepresentable: UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        
     }
 }
 
