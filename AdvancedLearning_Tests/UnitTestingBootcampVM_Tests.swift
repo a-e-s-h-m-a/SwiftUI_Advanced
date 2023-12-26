ed454fe752acf18e46403ece5ad418e651713f9c
Aeshma <madhushanka2727@gmail.com>
2023-12-26 12:01:52 +0530

    unit testing structure

---
+++ AdvancedLearning_Tests/UnitTestingBootcampVM_Tests.swift
@@ -0,0 +1,32 @@
+//
+//  UnitTestingBootcampVM_Tests.swift
+//  AdvancedLearning_Tests
+//
+//  Created by Malith Madhushanka on 2023-12-26.
+//
+
+import XCTest
+
+// Naming structure: test_UnitOfWork_StateUnderTest_expectedBehaviour
+
+// Testing structute: given when then
+
+final class UnitTestingBootcampVM_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_UnitTestingBootcampVM_isPremium_shouldBeTrue() {
+        // given
+        
+        // when
+        
+        // then 
+    }
+
+}
