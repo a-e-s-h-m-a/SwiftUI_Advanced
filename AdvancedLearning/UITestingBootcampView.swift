c1b0a322fb823d920de272fb70301a691e4aa54d
Aeshma <madhushanka2727@gmail.com>
2024-01-02 20:59:43 +0530

    UI testing setup

---
+++ AdvancedLearning/UITestingBootcampView.swift
@@ -0,0 +1,121 @@
+//
+//  UITestingBootcampView.swift
+//  AdvancedLearning
+//
+//  Created by Malith Madhushanka on 2023-12-30.
+//
+
+import SwiftUI
+
+class UITestingBootcampViewModel: ObservableObject {
+    let placeholderText: String = "Add your name..."
+    @Published var textFieldText: String = ""
+    @Published var currentUserIsSignedIn: Bool = false
+    
+    func signUpButtonPressed() {
+        guard !textFieldText.isEmpty else { return }
+        currentUserIsSignedIn = true
+    }
+}
+
+struct UITestingBootcampView: View {
+    
+    @StateObject private var vm = UITestingBootcampViewModel()
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.blue, Color.black]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            
+            ZStack {
+                if vm.currentUserIsSignedIn {
+                    SignedInHomeView()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .transition(.move(edge: .trailing))
+                }
+                if !vm.currentUserIsSignedIn {
+                    signUpLayer
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .transition(.move(edge: .leading))
+                }
+                
+            }
+        }
+    }
+}
+
+extension UITestingBootcampView {
+    private var signUpLayer: some View {
+        VStack {
+            TextField(vm.placeholderText, text: $vm.textFieldText)
+                .font(.headline)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+            
+            Button(action: {
+                withAnimation(.spring()) {
+                    vm.signUpButtonPressed()
+                }
+            }, label: {
+                Text("Sign Up")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+            })
+        }
+        .padding()
+    }
+}
+
+struct SignedInHomeView: View {
+    
+    @State private var showAlert: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Button(action: {
+                    showAlert.toggle()
+                }, label: {
+                    Text("Show welcome alert")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(10)
+                })
+                .alert(isPresented: $showAlert, content: {
+                    return Alert(title: Text("welcome to the app!"))
+                })
+                
+                NavigationLink {
+                    Text("Destination")
+                } label: {
+                    Text("Navigate")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+
+            }
+            .padding()
+            .navigationTitle("Welcome!")
+        }
+    }
+}
+
+#Preview {
+    UITestingBootcampView()
+}
