65681d213aa90506aa80e861b767b3a31cc188f8
Aeshma <madhushanka2727@gmail.com>
2024-03-03 22:28:35 +0530

    final

--- AdvancedLearning/ErrorAlertBootcamp.swift
+++ AdvancedLearning/ErrorAlertBootcamp.swift
@@ -7,18 +7,132 @@
 
 import SwiftUI
 
+protocol AppAlert {
+    var title: String { get }
+    var subTitle: String? { get }
+    var buttons: AnyView { get }
+}
+
+extension View {
+    func showCustomAlert<T: AppAlert>(alert: Binding<T?>) -> some View {
+        self
+            .alert(alert.wrappedValue?.title ?? "Error", isPresented: Binding(value: alert), actions: {
+                alert.wrappedValue?.buttons
+            }, message: {
+                if let subTitle = alert.wrappedValue?.subTitle {
+                    Text(subTitle)
+                }
+            })
+    }
+}
+
 struct ErrorAlertBootcamp: View {
     
-    @State private var errorTitle: String? = nil
+    @State private var alert: MyCustomAlert? = nil
     @State private var showError: Bool = false
     
     var body: some View {
         Button("CLICK ME!") {
             saveData()
         }
-        .alert(errorTitle ?? "Error", isPresented: Binding(value: $errorTitle)) { // from last video
-            Button("OK") {
-                
+        .showCustomAlert(alert: $alert)
+        
+        
+//        .alert(alert?.title ?? "Error", isPresented: Binding(value: $alert), actions: {
+//            alert?.getButtonsForAlert
+//        }, message: {
+//            if let subTitle = alert?.subTitle {
+//                Text(subTitle)
+//            }
+//        })
+        
+        
+//        .alert(error?.localizedDescription ?? "Error", isPresented: Binding(value: $error)) { // from last video
+//            Button("OK") {
+//                
+//            }
+//        }
+    }
+    
+//    // custom errors
+//    enum MyCustomError: Error, LocalizedError {
+//        case noInternetConnection
+//        case dataNotFound
+//        case urlError(error: Error)
+//        
+//        var errorDescription: String? {
+//            switch self {
+//            case .noInternetConnection:
+//                return "Please check your internet connection and try again."
+//            case .dataNotFound:
+//                return "No data found."
+//            case .urlError(error: let error):
+//                return "Error: \(error.localizedDescription)"
+//            }
+//        }
+//    }
+    
+    // custom errors
+    enum MyCustomAlert: Error, LocalizedError, AppAlert {
+        case noInternetConnection(onOKPressed: () -> (), onRetryPressed: () -> ())
+        case dataNotFound
+        case urlError(error: Error)
+        
+        var errorDescription: String? {
+            switch self {
+            case .noInternetConnection:
+                return "Please check your internet connection and try again."
+            case .dataNotFound:
+                return "No data found."
+            case .urlError(error: let error):
+                return "Error: \(error.localizedDescription)"
+            }
+        }
+        
+        var title: String {
+            switch self {
+            case .noInternetConnection:
+                return "No Internet connection"
+            case .dataNotFound:
+                return "No data"
+            case .urlError(error: let error):
+                return "Error"
+            }
+        }
+        
+        var subTitle: String? {
+            switch self {
+            case .noInternetConnection:
+                return "No Internet connection"
+            case .dataNotFound:
+                return nil
+            case .urlError(error: let error):
+                return "Error"
+            }
+        }
+        
+        var buttons: AnyView {
+            AnyView(getButtonsForAlert)
+        }
+        
+        @ViewBuilder
+        var getButtonsForAlert: some View {
+            switch self {
+            case .noInternetConnection(let onOKPressed, let onRetryPressed):
+                Button("OK") {
+                    onOKPressed()
+                }
+                Button("RETRY") {
+                    onRetryPressed()
+                }
+            case .dataNotFound:
+                Button("RETRY") {
+                    
+                }
+            default:
+                Button("Delete", role: .destructive) {
+                    
+                }
             }
         }
     }
@@ -30,8 +144,17 @@ struct ErrorAlertBootcamp: View {
             // do something
         } else {
             // error
-            errorTitle = "An error occured"
-            showError.toggle()
+            //errorTitle = "An error occured"
+            //showError.toggle()
+            
+           // let myError: Error = MyCustomAlert.urlError(error: URLError(.badURL))
+            
+            alert = .noInternetConnection(onOKPressed: {
+                
+            }, onRetryPressed: {
+                
+            })
+            
         }
     }
 }
