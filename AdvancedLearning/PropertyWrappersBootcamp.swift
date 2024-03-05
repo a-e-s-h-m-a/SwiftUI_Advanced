6c99d3df7260b6655ad055ba0a157a44f2f7e833
Aeshma <madhushanka2727@gmail.com>
2024-03-05 21:40:27 +0530

    refactor to use a struct

--- AdvancedLearning/PropertyWrappersBootcamp.swift
+++ AdvancedLearning/PropertyWrappersBootcamp.swift
@@ -7,37 +7,8 @@
 
 import SwiftUI
 
-struct PropertyWrappersBootcamp: View {
-    
-    @State private var title: String = "Starting title"
-    
-    var body: some View {
-        VStack(spacing: 30) {
-            Text(title).font(.largeTitle)
-            
-            Button("Click me 1") {
-                setTitle(newValue: "title 1")
-            }
-            Button("Click me 2") {
-                setTitle(newValue: "title 2")
-            }
-        }
-        .onAppear {
-            do {
-                let savedTitle = try String(contentsOf: path, encoding: .utf8)
-                title = savedTitle
-                print("SUCCESS READ")
-            } catch {
-                print("ERROR READ: \(error)")
-            }
-        }
-    }
-    
-    private func setTitle(newValue: String) {
-        let uppercased = newValue.uppercased()
-        title = uppercased
-        save(newValue: uppercased)
-    }
+struct FileManagerProperty {
+    var title: String = "Starting title"
     
     private var path: URL {
         FileManager
@@ -47,13 +18,44 @@ struct PropertyWrappersBootcamp: View {
             .appending(path: "custom_title.txt")
     }
     
-    private func save(newValue: String) {
+    mutating func load() {
+        do {
+            title = try String(contentsOf: path, encoding: .utf8)
+            print("SUCCESS READ")
+        } catch {
+            print("ERROR READ: \(error)")
+        }
+    }
+    
+    mutating func save(newValue: String) {
         do {
             try newValue.write(to: path, atomically: false, encoding: .utf8)
-            print(NSHomeDirectory())
+            title = newValue
             print("SUCCESS SAVE")
         } catch {
             print("ERROR SAVING \(error)")
+        }
+    }
+}
+
+struct PropertyWrappersBootcamp: View {
+    
+    @State var fileManagerProperty = FileManagerProperty()
+    //@State private var title: String = "Starting title"
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Text(fileManagerProperty.title).font(.largeTitle)
+            
+            Button("Click me 1") {
+                fileManagerProperty.save(newValue: "title 1")
+            }
+            Button("Click me 2") {
+                fileManagerProperty.save(newValue: "title 2")
+            }
+        }
+        .onAppear {
+            fileManagerProperty.load()
         }
     }
 }
