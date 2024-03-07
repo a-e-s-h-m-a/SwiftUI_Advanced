b98c65358d3b9fc14dbf2f78af68344a069a71be
Aeshma <madhushanka2727@gmail.com>
2024-03-07 21:29:47 +0530

    make streamable

--- AdvancedLearning/PropertyWrappersBootcamp2.swift
+++ AdvancedLearning/PropertyWrappersBootcamp2.swift
@@ -129,6 +129,86 @@ struct FileManagerValues {
     let userProfile = FileManagerKeypath(key: "user_profile", type: User.self)
 }
 
+import Combine
+
+@propertyWrapper
+struct FileManagerCodableStreamableProperty<T: Codable>: DynamicProperty {
+     // telling swiftUI that there is a dynamic property here
+    @State private var value: T?
+    let key: String
+    private let publisher: CurrentValueSubject<T?, Never>
+    
+    var wrappedValue: T? {
+        get { value }
+        nonmutating set {
+            save(newValue: newValue)
+        }
+    }
+    
+    var projectedValue: CurrentValueSubject<T?, Never> {
+        publisher
+    }
+    
+//    var projectedValue: Binding<T?> {
+//        Binding(
+//            get: { wrappedValue },
+//            set: { wrappedValue = $0 }
+//        )
+//    }
+    
+    init(_ key: String) {
+        self.key = key
+        
+        do {
+            let url = FileManager.documentPath(key: key)
+            let data = try Data(contentsOf: url)
+            let object = try JSONDecoder().decode(T.self, from: data)
+            
+            _value = State(wrappedValue: object)
+            publisher = CurrentValueSubject(object)
+            print("SUCCESS READ")
+        } catch {
+            _value = State(initialValue: nil)
+            publisher = CurrentValueSubject(nil)
+            print("ERROR READ: \(error)")
+        }
+    }
+    
+    init(_ key: KeyPath<FileManagerValues, FileManagerKeypath<T>>) {
+        let keyPath = FileManagerValues.shared[keyPath: key]
+        
+        let key = keyPath.key
+        self.key = key
+        
+        do {
+            let url = FileManager.documentPath(key: key)
+            let data = try Data(contentsOf: url)
+            let object = try JSONDecoder().decode(T.self, from: data)
+            
+            _value = State(wrappedValue: object)
+            publisher = CurrentValueSubject(object)
+            print("SUCCESS READ")
+        } catch {
+            _value = State(initialValue: nil)
+            publisher = CurrentValueSubject(nil)
+            print("ERROR READ: \(error)")
+        }
+    }
+    
+    private func save(newValue: T?) {
+        do {
+            let data = try JSONEncoder().encode(newValue)
+            try data.write(to: FileManager.documentPath(key: key))
+        
+            value = newValue
+            publisher.send(newValue)
+            print("SUCCESS SAVE")
+        } catch {
+            print("ERROR SAVING \(error)")
+        }
+    }
+}
+
 
 struct PropertyWrappersBootcamp2: View {
     
@@ -136,15 +216,22 @@ struct PropertyWrappersBootcamp2: View {
     @Uppercased private var title: String = "Hello world"
     //@FileManagerCodableProperty("user_profile") private var userProfile: User?
     //@FileManagerCodableProperty(\.userProfile) private var userProfile: User?
-    @FileManagerCodableProperty(\.userProfile) private var userProfile
+    //@FileManagerCodableProperty(\.userProfile) private var userProfile
+    @FileManagerCodableStreamableProperty(\.userProfile) private var userProfile
     
     var body: some View {
         VStack {
             Button(title) {
                 title = "new title"
             }
-            SomeBindingView(userProfile: $userProfile)
+            //SomeBindingView(userProfile: $userProfile)
+            Button(userProfile?.name ?? "no value") {
+                userProfile = User(name: "NICK", age: 27, isPremium: false)
+            }
         }
+        .onReceive($userProfile, perform: { newValue in
+            print("RECEIVED NEW VALUE OF: \(newValue)")
+        })
         .onAppear {
             print(NSHomeDirectory())
         }
